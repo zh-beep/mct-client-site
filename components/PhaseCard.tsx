4ba01645662fcdf08e156ed7796fb3f7bdbb9041
Zanir Habib <zanir.habib@gmail.com>
2025-12-17 18:17:41 -0600

    Change status from 'YOU ARE HERE' to 'IN PROGRESS'

--- components/PhaseCard.tsx
+++ components/PhaseCard.tsx
@@ -45,7 +45,7 @@ export default function PhaseCard({
             </span>
             {isCurrent && (
               <span className="px-3 py-1 bg-blue-500 text-white text-xs font-semibold rounded-full">
-                YOU ARE HERE
+                IN PROGRESS
               </span>
             )}
           </div>
